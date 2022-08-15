e6c07ba775ff1261d42151579861f7bd9f3c8588
David Wengier <david.wengier@microsoft.com>
2022-08-15 16:00:10 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.Collections;
+
+var path = Path.Combine(Environment.CurrentDirectory, "output.txt");
+var tempPath = path + ".tmp";
+
+var tempFile = new FileInfo(tempPath);
+
+if (tempFile.Exists)
+    tempFile.Delete();
+
+using (var writer = tempFile.CreateText())
+{
+    foreach (DictionaryEntry entry in Environment.GetEnvironmentVariables())
+    {
+        writer.WriteLine($"{entry.Key}: {entry.Value}");
+    }
+
+    var oldFileInfo = new FileInfo(path);
+    if (oldFileInfo.Exists)
+        oldFileInfo.Delete();
+}
+
+if (args.Length > 0)
+{
+    File.Move(tempPath, path);
+}
+else
+{
+    tempFile.MoveTo(path);
+}
